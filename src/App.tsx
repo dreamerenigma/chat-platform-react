a55af1fc2902b070b925e28a2ca10f2fab4290fd
Andrey <Hitmanki@yandex.ru>
2022-09-19 01:38:38 +0400

    Set up React Router & Routes

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Outlet, Route, Routes } from 'react-router-dom';
+
+function App() {
+	return (
+		<>
+			<Routes>
+				<Route path="/" element={<div>Hello, Home!</div>}></Route>
+				<Route
+					path="conversations"
+					element={
+					<div>
+						<div>Conversation</div>
+					</div>
+				}
+			>
+
+					<Route path=":id" element={<div>Conversation ID Page</div>} />
+				</Route>
+			</Routes>
+		</>
+	);
+}
+
+export default App;
