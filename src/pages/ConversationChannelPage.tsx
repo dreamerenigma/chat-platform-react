17afd16fee0b813519078d931754c18320a2f2d9
Andrey <Hitmanki@yandex.ru>
2022-10-01 19:19:48 +0400

    Navigate to conversation on click

--- src/pages/ConversationChannelPage.tsx
+++ src/pages/ConversationChannelPage.tsx
@@ -2,7 +2,6 @@ import { useParams } from "react-router-dom";
 import { ConversationChannelPageStyle } from "../utils/styles";
 
 export const ConversationChannelPage = () => {
-	console.log(useParams());
 	return (
 		<ConversationChannelPageStyle>Channel Page</ConversationChannelPageStyle>
 	);
--- src/pages/ConversationPage.tsx
+++ src/pages/ConversationPage.tsx
@@ -2,12 +2,14 @@ import { Outlet, useParams, useSearchParams } from "react-router-dom";
 import { ConversationPanel } from "../components/conversation/ConversationPanel";
 import { ConversationSidebar } from "../components/conversation/ConversationSidebar";
 import { Page } from "../utils/styles";
+import mockConversations from '../__mocks__/conversations';
 
 export const ConversationPage = () => {
 	const { id } = useParams();
+	console.log(id); 
 	return (
 		<Page>
-			<ConversationSidebar />
+			<ConversationSidebar conversations={mockConversations} />
 				{!id && <ConversationPanel />}
 			<Outlet />
 		</Page>
