384774444315a77484da678c2b5e82fb0665b8db
Andrey <Hitmanki@yandex.ru>
2022-10-03 21:42:19 +0400

    Invoke auth endpoint & protect

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import { Route, Routes } from 'react-router-dom';
+import React, { FC, useEffect, useState } from 'react';
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
+import { AuthenticateRoute } from './components/AuthenticatedRoute';
 import { ConversationChannelPage } from './pages/ConversationChannelPage';
 import { ConversationPage } from './pages/ConversationPage';
 import { LoginPage } from './pages/LoginPage';
 import { RegisterPage } from './pages/RegisterPage';
+import { getAuthUser } from './utils/api';
+import { User } from './utils/types';
 
 function App() {
 	return (
 		<>
 			<Routes>
-				<Route path="/register" element={<RegisterPage />}></Route>
-				<Route path="/login" element={<LoginPage />}></Route>
-				<Route path="conversations" element={<ConversationPage />}>
+				<Route path="/register" element={<RegisterPage />} />
+				<Route path="/login" element={<LoginPage />} />
+				<Route 
+					path="conversations" 
+					element={
+						<AuthenticateRoute>
+							<ConversationPage />
+						</AuthenticateRoute>
+					}
+				>
 					<Route path=":id" element={<ConversationChannelPage />} />
 				</Route>
 			</Routes>
--- src/pages/ConversationPage.tsx
+++ src/pages/ConversationPage.tsx
@@ -1,12 +1,15 @@
+import { useEffect } from "react";
 import { Outlet, useParams, useSearchParams } from "react-router-dom";
 import { ConversationPanel } from "../components/conversation/ConversationPanel";
 import { ConversationSidebar } from "../components/conversation/ConversationSidebar";
+import { getAuthUser } from "../utils/api";
 import { Page } from "../utils/styles";
 import mockConversations from '../__mocks__/conversations';
 
 export const ConversationPage = () => {
 	const { id } = useParams();
-	console.log(id); 
+	console.log(id);
+	
 	return (
 		<Page>
 			<ConversationSidebar conversations={mockConversations} />
