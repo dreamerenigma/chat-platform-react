8a929c328bb3804da0a48b6b00325ff1e3f6ad0e
Andrey <Hitmanki@yandex.ru>
2022-10-16 10:25:27 +0400

    add message input field functionality & call api to create message

--- src/components/messages/MessageInputField.tsx
+++ src/components/messages/MessageInputField.tsx
@@ -1,7 +1,25 @@
+import { Dispatch, FC, SetStateAction } from "react";
 import { MessageInput, MessageInputContainer } from "../../utils/styles";
 
-export const MessageInputField = () => {
-	return <MessageInputContainer>
-		<MessageInput />
-	</MessageInputContainer>;
+type Props = {
+	content: string;
+	setContent: Dispatch<SetStateAction<string>>;
+	sendMessage: (e: React.FormEvent<HTMLFormElement>) => void;
+};
+
+export const MessageInputField: FC<Props> = ({ 
+	content, 
+	setContent,
+	sendMessage,
+}) => {
+	return (
+		<MessageInputContainer>
+			<form onSubmit={sendMessage}>
+				<MessageInput 
+					value={content} 
+					onChange={(e) => setContent(e.target.value)}
+				/>
+			</form>
+		</MessageInputContainer>
+	);
 };
