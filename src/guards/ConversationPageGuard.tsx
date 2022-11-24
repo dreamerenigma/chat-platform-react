4aa5c72b57802f93573538cc7dbd8f4c48566e11
Andrey <Hitmanki@yandex.ru>
2022-11-24 22:28:16 +0400

    redirect user when accesing a group they have no permisiions for

--- src/guards/ConversationPageGuard.tsx
+++ src/guards/ConversationPageGuard.tsx
@@ -5,10 +5,10 @@ import { useConversationGuard } from "../utils/hooks/useConversationGuard";
 export const ConversationPageGuard: FC<React.PropsWithChildren> = ({
 	children,
 }) => {
-	const { id } = useParams(); 
 	const location = useLocation();
 	const { loading, error } = useConversationGuard();
 	if (loading) return <div>loading conversation</div>;
+
 	return error ? (
 		<Navigate to="/login" state={{ from: location }} replace />
 	) : (
---
+++ src/guards/GroupPageGuard.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+import { useLocation, Navigate } from "react-router-dom";
+import { useConversationGuard } from "../utils/hooks/useConversationGuard";
+
+export const GroupPageGuard: FC<React.PropsWithChildren> = ({ children }) => {
+	const location = useLocation();
+	const { loading, error } = useConversationGuard();
+	if (loading) return <div>loading group</div>;
+
+	return error ? (
+		<Navigate to="/groups" state={{ from: location }} replace />
+	) : (
+		<>{children}</>
+	);
+};
