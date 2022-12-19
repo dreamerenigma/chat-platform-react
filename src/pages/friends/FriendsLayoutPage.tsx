5271474d10ce759fd988f5aabbae95f3885dace3
Andrey <Hitmanki@yandex.ru>
2022-12-19 13:34:37 +0400

    render add friend button on navbar

--- src/pages/friends/FriendsLayoutPage.tsx
+++ src/pages/friends/FriendsLayoutPage.tsx
@@ -6,6 +6,8 @@ import {
 	FriendsPageStyle, 
 } from "../../utils/styles/friends";
 import { FriendsPage } from "./FriendsPage";
+import { Button } from "../../utils/styles";
+import { AiOutlineUserAdd } from "react-icons/ai";
 
 export const FriendsLayoutPage  = () => {
 	const { pathname } = useLocation();
@@ -13,17 +15,23 @@ export const FriendsLayoutPage  = () => {
 	return(
 		<FriendsPageStyle>
 			<FriendsNavbar>
-				{friendsNavbarItems.map((item) => (
-					<FriendsNavbarItem 
-						key={item.id} 
-						active={pathname === item.pathname}
-						onClick={() => navigate(item.pathname)}
-					>
-						{item.label}
-					</FriendsNavbarItem>
-				))}
+				<div className="navLinks">
+					{friendsNavbarItems.map((item) => (
+						<FriendsNavbarItem 
+							key={item.id} 
+							active={pathname === item.pathname}
+							onClick={() => navigate(item.pathname)}
+						>
+							{item.label}
+						</FriendsNavbarItem>
+					))}
+				</div>
+				<Button size="sm" flex={true} variant="primary" >
+						<AiOutlineUserAdd size={24}/>
+						<span>Add Friend</span>
+				</Button>
 			</FriendsNavbar>
-				{pathname === '/friends' && <FriendsPage />}
+			{pathname === '/friends' && <FriendsPage />}
 			<Outlet />
 		</FriendsPageStyle>
 	);
---
+++ src/utils/styles/button.tsx
@@ -0,0 +1,84 @@
+import styled, { css } from "styled-components";
+
+type Size = 'sm' | 'md' | 'lg';
+type ButtonVariant = 'primary' | 'outline';
+
+type  ButtonProps = Partial<{
+	size: Size;
+	variant: ButtonVariant;
+	flex: boolean;
+}>;
+
+export const getButtonSizeStyle = (size?: Size) => {
+	switch (size) {
+		case 'sm':
+			return css`
+				padding: 10px 20px;
+				font-size: 16px;
+			`;
+		case 'md':
+			return css`
+				padding: 14px 26px;
+				font-size: 18px;
+			`;
+		case 'lg':
+			return css`
+				padding: 14px 26px;
+				font-size: 16px;
+			`;
+		default:
+			return css`
+			padding: 12px 24px;
+			font-size: 16px;
+		`;
+	}
+};
+
+export const getButttonVariantStyle = (variant?: ButtonVariant) => {
+	const primary = css`
+		background-ciolor: #2b09ff;
+		color: #fff;
+		&:hover {
+			cursor: pointer;
+			background-color: #3415ff;
+		}
+		&:active {
+			background-color: #3a1cff;
+		}
+		&:disabled {
+			background-color: #4937bc7c;
+			color: #878787a2;
+			cursor: not-allowed;
+		}
+	`;
+	const outline = css`
+		background-color: inherit;
+		border: 1px solid #2b09ff;
+		color: #fff;
+	`;
+	switch (variant) {
+		case 'primary':
+			return primary;
+		case 'outline':
+			return outline;
+		default:
+			return primary;
+	}
+};
+
+export const Button = styled.button<ButtonProps>`
+	outline: none;
+	border: none;
+	font-family: 'Inter';
+	border-radius: 10px;
+	font-weight: 500;
+	transition: 250ms background-color ease;
+	${({ size }) => getButtonSizeStyle(size)}
+	${({ variant }) => getButttonVariantStyle(variant)}
+	${({ flex }) => 
+		flex && 
+		css`
+			display: flex;
+			align-items: center;
+	`}
+`;
