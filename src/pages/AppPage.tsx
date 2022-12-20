254631bb9f7c758901cea1daea5c89931f6bf9bb
Andrey <Hitmanki@yandex.ru>
2022-12-20 21:05:27 +0400

    add clickable toast when friend request is received

--- src/pages/AppPage.tsx
+++ src/pages/AppPage.tsx
@@ -1,20 +1,32 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { UserSidebar } from "../components/sidebars/UserSidebar"
-import { LayoutPage } from "../utils/styles";
 import { useContext, useEffect } from "react";
 import { SocketContext } from "../utils/context/SocketContent";
-import { AcceptFriendRequestResponse, FriendRequest } from "../utils/types";
 import { AppDispatch } from "../store";
 import { useDispatch } from "react-redux";
-import { addFriendRequest, removeFriendRequest } from "../store/friends/friendsSlice";
+import { 
+	addFriendRequest, 
+	removeFriendRequest, 
+} from "../store/friends/friendsSlice";
+import { LayoutPage } from "../utils/styles";
+import { AcceptFriendRequestResponse, FriendRequest } from "../utils/types";
+import { IoMdPersonAdd } from 'react-icons/io';
+import { useToast } from "../utils/hooks/useToast";
 
 export const AppPage = () => {
 	const socket = useContext(SocketContext);
 	const dispatch = useDispatch<AppDispatch>();
+	const navigate = useNavigate();
+	const { info } = useToast({ theme: 'dark' });
 	useEffect(() => {
 		socket.on('onFriendRequestReceived', (payload: FriendRequest) => {
 			console.log('onFriendRequestReceived');
 			console.log(payload);
+			info('Incoming Friend Request', { 
+				position: 'bottom-left', 
+				icon: IoMdPersonAdd,
+				onClick: () => navigate('/friends/requests'),
+			});
 			dispatch(addFriendRequest(payload));
 		});
 
--- src/utils/hooks/useToast.tsx
+++ src/utils/hooks/useToast.tsx
@@ -1,11 +1,13 @@
 import { toast, ToastOptions } from 'react-toastify';
 
-export function useToast(defaultOptions: ToastOptions<{}> = { theme: 'dark' }) {
-	const success = (data: string) =>
+export function useToast(defaultOptions?: ToastOptions<{}>) {
+	const success = (data: string) => 
 		toast(data, { ...defaultOptions, type: 'success' });
 
 	const error = (data: string) => 
 		toast(data, { ...defaultOptions, type: 'error' });
 
-	return { success, error };
+	const info = (data: string, options?: ToastOptions<{}>) => 
+		toast(data, { ...defaultOptions, type: 'info' });
+	return { success, error, info };
 }
