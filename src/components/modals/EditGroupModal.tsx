963490bdf04a16fd82d594b9aa2f26d5e810d648
Andrey <Hitmanki@yandex.ru>
2023-01-22 21:13:53 +0400

    disable edit group modal from being closed when changes are saving

--- src/components/modals/EditGroupModal.tsx
+++ src/components/modals/EditGroupModal.tsx
@@ -1,8 +1,8 @@
 import { createRef } from "react";
 import { setShowEditGroupModal } from "../../store/groupSlice";
 import { useKeydown } from "../../utils/hooks";
-import { useDispatch } from "react-redux";
-import { AppDispatch } from "../../store";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../store";
 import { DivMouseEvent } from "../../utils/types";
 import { MdClose } from "react-icons/md";
 import { ModalContainer, ModalHeader, ModalContentBody } from ".";
@@ -12,13 +12,19 @@ import { EditGroupForm } from "../forms/EditGroupForm";
 export const EditGroupModal = () => {
 	const ref = createRef<HTMLDivElement>();
    const dispatch = useDispatch<AppDispatch>();
+	const { isSavingChanges } = useSelector((state: RootState) => state.groups);
+	
 	const handleOverlayClick = (e: DivMouseEvent) =>
       ref.current &&
       ref.current === e.target &&
+		!isSavingChanges &&
       dispatch(setShowEditGroupModal(false));
 
    useKeydown(
-      (e) => e.key === 'Escape' && dispatch(setShowEditGroupModal(false))
+      (e) => 
+		e.key === 'Escape' && 
+		!isSavingChanges &&
+		dispatch(setShowEditGroupModal(false))
    );
 
 	return (
@@ -28,7 +34,10 @@ export const EditGroupModal = () => {
 					<h2>Edit Group</h2>
 					<MdClose 
                   size={32} 
-                  onClick={() => dispatch(setShowEditGroupModal(false))}
+                  onClick={() => 
+							!isSavingChanges && dispatch(setShowEditGroupModal(false))
+						}
+						cursor={isSavingChanges ? 'not-allowed' : 'pointer'}
                />
 				</ModalHeader>
 				<ModalContentBody>
